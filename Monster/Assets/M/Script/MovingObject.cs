af8f11ca9efef42a2c17ea5afbaaefdc10ef6d41
ljmin0131 <ljmin0131@gmail.com>
2022-11-17 23:33:52 +0900

    first_monster
    11 17

---
+++ Monster/Assets/M/Script/MovingObject.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+
+    public string currentMapName;
+
+    private BoxCollider2D boxCollider;
+    public LayerMask layerMask;  //통과가 불가능한 레이어 (가상 벽)
+    public float speed; 
+    
+    private Vector3 vector;  
+
+    public float runSpeed; 
+    private float applyRunSpeed;
+    private bool applyRunFlag = false;
+
+    public int walkCount; 
+    private int currentWalkCount;
+
+    private bool canMove = true; 
+
+    private Animator animator;
+
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        boxCollider = GetComponent<BoxCollider2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    IEnumerator MoveCoroutine()
+    {
+        while (Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                applyRunSpeed = runSpeed;
+                applyRunFlag = true;
+            }
+            else
+            {
+                applyRunSpeed = 0;
+                applyRunFlag = false;
+            }
+
+            vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z); 
+
+            if (vector.x != 0) 
+                vector.y = 0;
+
+            animator.SetFloat("DirX", vector.x);
+            animator.SetFloat("DirY", vector.y);
+
+            RaycastHit2D hit; 
+
+            Vector2 start = transform.position; 
+            Vector2 end = start + new Vector2(vector.x * speed * walkCount, vector.y * speed * walkCount); 
+
+            boxCollider.enabled = false;
+            hit = Physics2D.Linecast(start, end, layerMask);
+            boxCollider.enabled = true;
+
+            if (hit.transform != null)
+                break;
+
+            animator.SetBool("Walking", true);
+
+            while (currentWalkCount < walkCount)
+            {
+                if (vector.x != 0)
+                {
+                    transform.Translate(vector.x * (speed + applyRunSpeed), 0, 0); 
+                }
+                else if (vector.y != 0)
+                {
+                    transform.Translate(0, vector.y * (speed + applyRunSpeed), 0);
+                }
+
+                if (applyRunFlag)
+                {
+                    currentWalkCount++;
+                }
+                currentWalkCount++;
+                yield return new WaitForSeconds(0.01f);
+            }
+            currentWalkCount = 0;
+
+        }
+
+        animator.SetBool("Walking", false); 
+
+        canMove = true;
+    }
+
+    
+    void Update()
+    {
+        if (canMove)
+        {
+            if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+            
+            {
+                canMove = false; 
+                StartCoroutine(MoveCoroutine());
+            }
+        }
+
+    }
+}
