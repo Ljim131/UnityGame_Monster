c929965e20d735b686c7c7df1f27cfac12501d87
ljmin0131 <ljmin0131@gmail.com>
2022-11-22 00:14:30 +0900

    9_Monster
    Camera, Map

--- Monster/Assets/M/Script/CameraManager.cs
+++ Monster/Assets/M/Script/CameraManager.cs
@@ -9,9 +9,20 @@ public class CameraManager : MonoBehaviour
     public float moveSpeed; 
     private Vector3 targetPosition; 
 
-    // Start is called before the first frame update
-    void Start(){
-        if(instance != null)
+    public BoxCollider2D bound;
+
+    private Vector3 minBound;
+    private Vector3 maxBound;
+
+    private float halfWidth;
+    private float halfHeight;
+
+    private Camera theCamera;
+
+
+    private void Awake()
+    {
+        if (instance != null)
         {
             Destroy(this.gameObject);
         }
@@ -20,16 +31,37 @@ public class CameraManager : MonoBehaviour
             DontDestroyOnLoad(this.gameObject);
             instance = this;
         }
-        
     }
 
-    // Update is called once per frame
+
+
+    void Start(){
+        theCamera = GetComponent<Camera>();
+        minBound = bound.bounds.min;
+        maxBound = bound.bounds.max;
+        halfHeight = theCamera.orthographicSize;
+        halfWidth = halfHeight * Screen.width / Screen.height;
+    }
+
+    
     void Update(){
         if(target.gameObject != null)
         {
             targetPosition.Set(target.transform.position.x, target.transform.position.y, this.transform.position.z);
             this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        
+            float clampedX = Mathf.Clamp(this.transform.position.x, minBound.x + halfWidth, maxBound.x - halfWidth);
+            float clampedY = Mathf.Clamp(this.transform.position.y, minBound.y + halfHeight, maxBound.y - halfHeight);
+
+            this.transform.position = new Vector3(clampedX, clampedY, this.transform.position.z);
+        
         }
+    }
+    public void SetBound(BoxCollider2D newBound)
+    {
+        bound = newBound;
+        minBound = bound.bounds.min;
+        maxBound = bound.bounds.max;
     }
 
 }
--- Monster/Assets/M/Script/TransferMap.cs
+++ Monster/Assets/M/Script/TransferMap.cs
@@ -8,6 +8,7 @@ public class TransferMap : MonoBehaviour {
     public string transferMapName; //이동할 맵의 이름
 
     public Transform target;
+    public BoxCollider2D targetBound;
 
     private MovingObject thePlayer;
     private CameraManager theCamera;
@@ -20,7 +21,7 @@ public class TransferMap : MonoBehaviour {
         if(collision.gameObject.name == "Player") 
         {
             thePlayer.currentMapName = transferMapName;
-            //SceneManager.LoadScene(transferMapName);
+            theCamera.SetBound(targetBound);
             theCamera.transform.position = new Vector3(target.transform.position.x, target.transform.position.y, theCamera.transform.position.z);
             thePlayer.transform.position = target.transform.position;
         }
